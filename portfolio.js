77f3bdb0d64d5bb4734d01b164a43e618e6f2463
BLBaylis <bradleybaylis@hotmail.com>
2017-12-31 15:06:15 +0000

    v0.4.1
    Added projects nav bar animation and changed the close icon from an svg to a tranformation of the hamburger menu.  Need to animate this tranformation.

--- portfolio.js
+++ portfolio.js
@@ -1,62 +1,41 @@
-//Main Nav Bar
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("dropdown-links-div").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown-btn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-links-div");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
 //Side nav-bar
 
 function sideNavBar() {
 
+  var navbarClassList = document.getElementById("project-nav-bar").classList;
+  var iconClassList1 = document.getElementsByClassName("menu-icon")[0].classList;
+  var iconClassList2 = document.getElementsByClassName("menu-icon")[2].classList;
+
   function openNav() {
-    document.getElementById("project-nav-bar").style.left = "0";
-    document.getElementsByClassName("menu-open-icon")[0].style.display = "none";
-    document.getElementsByClassName("menu-open-icon")[1].style.display = "none";
-    document.getElementsByClassName("menu-open-icon")[2].style.display = "none";
-    document.getElementsByClassName("menu-close-icon")[0].style.display = "initial";
+    navbarClassList.add("nav-animate");
+    document.getElementsByClassName("menu-icon-container")[0].style.display = "initial";
+    document.getElementsByClassName("menu-icon")[1].style.display = "none";
+    iconClassList1.add("menu-icon-animate1");
+    iconClassList2.add("menu-icon-animate2");
     open = true;
   }
 
 
   function closeNav() {
-    document.getElementById("project-nav-bar").style.left = "-33%";
-    document.getElementsByClassName("menu-open-icon")[0].style.display = "flex";
-    document.getElementsByClassName("menu-open-icon")[1].style.display = "flex";
-    document.getElementsByClassName("menu-open-icon")[2].style.display = "flex";
-    document.getElementsByClassName("menu-close-icon")[0].style.display = "none";
+    navbarClassList.remove("nav-animate");
+    document.getElementsByClassName("menu-icon-container")[0].style.display = "flex";
+    document.getElementsByClassName("menu-icon")[1].style.display = "initial";
+    iconClassList1.remove("menu-icon-animate1");
+    iconClassList2.remove("menu-icon-animate2");
     open = false;
   }
 
   if (!open){
-    console.log(open);
     openNav();
-    console.log(open);
   } else {
-    console.log(open);
     closeNav();
-    console.log(open);
+
   }
 
 }
 
-var open = false;
+  var open = false;
+
 
 function activeBtn (btn) {
   var arr = document.getElementsByClassName("project-nav-item");
